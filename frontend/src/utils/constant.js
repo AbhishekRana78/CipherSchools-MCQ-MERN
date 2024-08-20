f3910c762caafefe5dd3ca3a1d21e7ccf9622932
AbhishekRana78 <abhishekrana8788@gmail.com>
2024-08-20 23:22:28 +0530

    succss

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const API_END_POINT = "http://localhost:8000/api/v1/user";
+export const API_END_POINT = "https://cipher-schools-mcq-mern.vercel.app/api/v1/user";
 
 // export const options = {
 //     method: 'GET',
